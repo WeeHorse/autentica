9803efb9da91d82f2927ddb941efeafff12fd1e0
Benjamin Berglund <benjaminberglund@gmail.com>
2022-04-07 15:32:59 +0200

    client example 1.0

--- src/main/resources/public/page-components/create-malaki.js
+++ src/main/resources/public/page-components/create-malaki.js
@@ -1,5 +1,26 @@
 class CreateMalaki extends Component{
 
+    events(){
+        $('body').on('submit', '#create-malaki', this.create)
+    }
+
+    async create(event){
+        event.preventDefault()
+        
+        let result = await fetch(apiHost + '/api/malakis', {
+            method: 'post',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + user.accessToken
+            },
+            body: JSON.stringify({
+                "name": document.querySelector('#malaki-name').value
+            })
+        })
+        let data = await result.json()
+        console.log(result, data)
+    }
+
     get template(){
         return `
             <h1>Create your Malaki, sir</h1>
